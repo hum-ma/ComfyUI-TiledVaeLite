5285b30042e4346b6b6c97ecbce6eaa85ecdbcfe
hum_ma <hum_ma@eclipso.eu>
2025-12-10 17:25:55 +0200

    Add files via upload
    Now even more lite; deleted DecoderNoise because it wasn't having any effect

--- __init__.py
+++ __init__.py
@@ -1,13 +1,10 @@
-from .decoder_noise import DecoderNoise
 from .tiled_vae_decode import LTTiledVAEDecode
 
 NODE_CLASS_MAPPINGS = {
-    "DecoderNoise": DecoderNoise,
     "LTTiledVAEDecode": LTTiledVAEDecode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "DecoderNoise": "🅛🅣 VAE Decoder Noise",
     "LTTiledVAEDecode": "🅛🅣 Tiled VAE Decode",
 }
 
